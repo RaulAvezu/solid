1375fa5f0a7ac2712d7b2d1e0d4caeacc5b40fad
RaulAvezu <raul.taqua@hotmail.com>
2022-06-27 23:49:19 -0300

    Heranças indesejadas

---
+++ 2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Tercerizados.java
@@ -0,0 +1,41 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+
+public class Tercerizados {
+	private DadosPessoais dadosPessoais;
+	private String empresa;
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
+	}
+
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
+	}
+
+	public String getNome() {
+		return dadosPessoais.getNome();
+	}
+
+	public String getCpf() {
+		return dadosPessoais.getCpf();
+	}
+
+	public Cargo getCargo() {
+		return dadosPessoais.getCargo();
+	}
+
+	public BigDecimal getSalario() {
+		return dadosPessoais.getSalario();
+	}
+
+}
--- 2051-solid-java-projeto_inicial/src/br/com/alura/rh/service/ValidacaoPercentualReajuste.java
+++ 2051-solid-java-projeto_inicial/src/br/com/alura/rh/service/ValidacaoPercentualReajuste.java
@@ -6,9 +6,9 @@ import java.math.RoundingMode;
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
-public class ValidacaoPercentualReajuste implements ValidacaoReajuste{
+public class ValidacaoPercentualReajuste implements ValidacaoReajuste {
 	public void validar(Funcionario funcionario, BigDecimal aumento) {
-		BigDecimal salarioAtual = funcionario.getSalario();
+		BigDecimal salarioAtual = funcionario.getDadosPessoais().getSalario();
 		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
 		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
